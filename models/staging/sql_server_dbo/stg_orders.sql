ac46fb659d3762a880381d30b76736f8caa46514
Jairo Luis Abril Moya <flooky8336@gmail.com>
2023-11-15 22:24:04 +0000

    pruebas test dia 3 final

--- models/staging/sql_server_dbo/stg_orders.sql
+++ models/staging/sql_server_dbo/stg_orders.sql
@@ -13,20 +13,22 @@ WITH src_orders AS (
 renamed_casted AS (
     SELECT
           order_id
-        ,tracking_id
-        , user_id 
-        , created_at
-        , address_id
-        , estimated_delivery_at
-        , delivered_at 
-        , shipping_service
-        , status
-        , promo_id
-        , shipping_cost
-        , order_cost
-        , order_total
-        , _fivetran_synced AS date_load
+          , tracking_id
+          , user_id
+          , created_at
+          , address_id
+          , estimated_delivery_at
+          , delivered_at
+          , CASE
+              WHEN status = 'preparing' THEN 'undefined'
+              ELSE COALESCE(shipping_service, 'undefined')
+            END AS shipping_service
+          , status
+          , promo_id
+          , shipping_cost
+          , order_cost
+          , order_total
+          , _fivetran_synced AS date_load
     FROM src_orders
-    )
-
+)
 SELECT * FROM renamed_casted
